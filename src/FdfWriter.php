7a3037e0e7746e42cf1991811506cf4021e0bdb0
ixarlie <ixarlie@gmail.com>
2025-06-25 00:09:01 +0200

    fix phpcs issues

--- src/FdfWriter.php
+++ src/FdfWriter.php
@@ -85,7 +85,7 @@ class FdfWriter implements \Stringable
     public static function escapePdfString(?string $string): string
     {
         // Create UTF-16BE string encode as ASCII hex
-        $string ??= $string;
+        $string ??= '';
         $utf16Value = mb_convert_encoding($string, 'UTF-16BE', 'UTF-8');
         if (false === $utf16Value) {
             return $string;
